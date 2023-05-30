a0a668ebc5c4927c9ac5c35baca2d17fadb96c48
Michel Pech <michelhphz@hotmail.com>
2023-05-30 15:46:10 -0300

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BirtAPI
-# BirtAPI
+
+-> How to use?
+
+After cloning the repository, you must download the Birt Designer for build your reports.
+https://eclipse-birt.github.io/birt-website/docs/installation/
+
+Save your reports on path "reports". For call, you must send a JSON for API with name of your report and parameters.
+
+You can use this JSON for look the results:
+{
+    "report": "demo.rptdesign",
+    "p_test": "test1234"
+}
+
+-> WARNING!
+
+You must open the XML Source of your report and change the property "version" (line 2) for "3.2.23".
+If you not do this, the report will presents errors.
---
+++ src/main/java/com/birt/api/BirtReportApplication.java
@@ -0,0 +1,93 @@
+package com.birt.api;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.birt.core.framework.Platform;
+import org.eclipse.birt.report.engine.api.EngineConfig;
+import org.eclipse.birt.report.engine.api.IReportEngine;
+import org.eclipse.birt.report.engine.api.IReportEngineFactory;
+import org.eclipse.birt.report.engine.api.IReportRunnable;
+import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
+import org.eclipse.birt.report.engine.api.PDFRenderOption;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.icu.util.Calendar;
+
+@RestController
+@SpringBootApplication
+public class BirtReportApplication {
+
+    @Value("${reports.relative.path}")
+    private String reportsPath;
+    @Value("${images.relative.path}")
+    private String imagesPath;	
+	@Value("${temp.files.path}")
+	private String tempPath;      
+    
+    private ApplicationContext context;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BirtReportApplication.class, args);
+	}
+
+	@SuppressWarnings("unchecked")
+	@PostMapping("/report")
+    public ResponseEntity<byte[]> gerarRelatorio(@RequestBody String json) {
+        try {
+			// Pega os parametros
+			ObjectMapper objectMapper = new ObjectMapper();
+			Map<String, String> parametersMap = objectMapper.readValue(json, Map.class);        	
+        	
+            // Configurações do BIRT
+			EngineConfig config = new EngineConfig();
+			config.getAppContext().put("spring", this.context);
+			Platform.startup(config);
+			IReportEngineFactory factory = (IReportEngineFactory) Platform.createFactoryObject(IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
+			IReportEngine engine = factory.createReportEngine(config);
+
+			// Carregar o design do relatório
+			IReportRunnable report = engine.openReportDesign(reportsPath + parametersMap.get("report"));
+
+			// Configurar os parâmetros
+			IRunAndRenderTask task = engine.createRunAndRenderTask(report);
+			for (Entry<String, String> parametro : parametersMap.entrySet()) {
+				task.setParameterValue(parametro.getKey(), parametro.getValue());
+			}
+
+            // Gerar o relatório em PDF
+			String reportFileName = tempPath + Calendar.getInstance().getTimeInMillis() + ".pdf";
+			
+			PDFRenderOption options = new PDFRenderOption();
+			options.setOutputFormat("pdf");
+			options.setOutputFileName(reportFileName);
+			task.setRenderOption(options);
+			task.run();
+			task.close();			
+
+            // Ler o arquivo do relatório gerado
+            byte[] relatorioBytes = Files.readAllBytes(Paths.get(reportFileName)); // Defina o caminho do arquivo gerado
+
+            // Remover arquivos temporários, se necessário
+            Files.deleteIfExists(Paths.get(reportFileName));
+
+            return ResponseEntity.ok()
+                    .header("Content-Type", "application/pdf")
+                    .body(relatorioBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
